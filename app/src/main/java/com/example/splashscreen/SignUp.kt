31b2936b5ec5b0fbd56648f2520e4ae5bffff5ac
arunsinghh <arunsinghh.dev@gmail.com>
2024-05-29 13:16:57 +0530

    SignUp

--- app/src/main/java/com/example/splashscreen/SignUp.kt
+++ app/src/main/java/com/example/splashscreen/SignUp.kt
@@ -3,14 +3,12 @@ package com.example.splashscreen
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_signup)
 
 
     }
